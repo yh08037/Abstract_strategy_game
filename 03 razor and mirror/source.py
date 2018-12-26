e333df4ff568eab4b824485d0d5309759e301093
Greathoney <44860482+Greathoney@users.noreply.github.com>
2018-12-27 01:45:40 +0900

    Add files via upload

--- 03 razor and mirror/source.py
+++ 03 razor and mirror/source.py
@@ -1,10 +1,22 @@
 import sys
 import pygame
 from pygame.locals import *
+import math
 
 font = 'myfont.ttf'
 
+def angle(num):
+    return math.tan(math.pi/180 * num)
+
+def razor_delta(direction):
+    return [20*math.cos(math.pi/180 * direction*60), 20*math.sin(math.pi/180 * direction*60)]
+
+def mirror_delta(direction):
+    return [-20*math.cos(math.pi/180 * direction*30), 20*math.sin(math.pi/180 * direction*30)]
+
+
 colorRed = (255, 0, 0)
+colorDarkGray = (49, 51, 53)
 colorBlue = (0, 0, 255)
 colorWhite = (255, 255, 255)
 colorGray = (127, 127, 127)
@@ -65,8 +77,6 @@ pygame.init()
 pygame.display.set_caption("Mirror and Razor")
 screen = pygame.display.set_mode(resolution)
 
-
-
 razor = []
 mirror = []
 
@@ -85,10 +95,10 @@ while True:
         if event.type == pygame.MOUSEMOTION:
             mousePos = event.pos
 
-    screen.fill(colorBlack)
+    screen.fill(colorDarkGray)
 
     if current == 'menu':
-        if leftMouseClicked and 610 < mousePos[0] < 670 and  400 < mousePos[1] < 440:
+        if leftMouseClicked and 610 < mousePos[0] < 670 and 400 < mousePos[1] < 440:
             current = 'game'
             continue
         if leftMouseClicked and 610 < mousePos[0] < 670 and 460 < mousePos[1] < 500:
@@ -135,43 +145,114 @@ while True:
 
         for i in boardPositionAll:
             for j in i:
-                if j[0] - 5 < mousePos[0] < j[0] + 5 and j[1] - 5 < mousePos[1] < j[1] + 5:
-                    pygame.draw.circle(screen, colorGray, (round(j[0]), round(j[1])), 10, 0)
-                if j in mirror:
-                    pygame.draw.circle(screen, colorBlue, (round(j[0]), round(j[1])), 16, 2)
-                elif j in razor:
-                    pygame.draw.circle(screen, colorRed, (round(j[0]), round(j[1])), 16, 2)
+                if currentGameSetting == 'position' and j[0] - 5 < mousePos[0] < j[0] + 5 and j[1] - 5 < mousePos[1] < j[1] + 5:
+                    if j in razor and j in mirror:
+                        pygame.draw.circle(screen, colorGray, (round(j[0]), round(j[1])), 10, 0)
+
                 pygame.draw.circle(screen, colorWhite, (round(j[0]), round(j[1])), 5, 0)
+        for i in razor:
+            pygame.draw.circle(screen, colorRed, (round(i[0]), round(i[1])), 15, 2)
+            if len(i) >= 3:
+                pygame.draw.aaline(screen, colorRed, i[0:2], list(map(lambda x,y:x+y, i[0:2], razor_delta(i[2]))))
+            if len(i) == 4:
+                pygame.draw.aaline(screen, colorRed, i[0:2], list(map(lambda x,y:x+y, i[0:2], razor_delta(i[3]))))
+        for i in mirror:
+            if len(i) == 3:
+                pygame.draw.aaline(screen, colorBlue, list(map(lambda x,y:x-y, i[0:2], mirror_delta(i[2]))), list(map(lambda x,y:x+y, i[0:2], mirror_delta(i[2]))))
+
 
         fontOrder = pygame.font.Font(font, 30)
         if currentGame == 'razor':
             textOrder = fontOrder.render('Razor turns', True, colorWhite)
         else:
             textOrder = fontOrder.render('Mirror turns', True, colorWhite)
-        textPlayOrder = textOrder.get_rect()
-        textPlayOrder.center = (1100, 100)
-        screen.blit(textOrder, textPlayOrder)
+        textOrderRect = textOrder.get_rect()
+        textOrderRect.center = (1100, 100)
+        screen.blit(textOrder, textOrderRect)
 
-        if currentGame == 'razor':
-            for i in boardPositionAll:
-                for j in i:
-                    if currentGameSetting = 'position':
-                        if leftMouseClicked and j[0] - 5 < mousePos[0] < j[0] + 5 and j[1] - 5 < mousePos[1] < j[1] + 5:
-                            if not (j in razor and j in mirror):
-                                razor.append(j)
-                                currentGame = 'mirror'
+        fontOrderExplanation = pygame.font.Font(font, 25)
+        if currentGameSetting == 'position':
+            textOrderExplanation = fontOrderExplanation.render('Choose a position.', True, colorWhite)
+        else:
+            textOrderExplanation = fontOrderExplanation.render('Choose a direction', True, colorWhite)
+        textOrderExplanationRect = textOrderExplanation.get_rect()
+        textOrderExplanationRect.center = (1070, 140)
+        screen.blit(textOrderExplanation, textOrderExplanationRect)
 
-        elif currentGame == 'mirror':
-            for i in boardPositionAll:
-                for j in i:
-                    if leftMouseClicked and j[0] - 5 < mousePos[0] < j[0] + 5 and j[1] - 5 < mousePos[1] < j[1] + 5:
-                        if not (j in razor and j in mirror):
-                            mirror.append(j)
-                            currentGame = 'razor'
+        if leftMouseClicked:
+            if currentGame == 'razor':
+                if currentGameSetting == 'position':
+                    for i in boardPositionAll:
+                        for j in i:
+                            if j[0] - 5 < mousePos[0] < j[0] + 5 and j[1] - 5 < mousePos[1] < j[1] + 5:
+                                if not (j in razor and j in mirror):
+                                    razor.append(list(j))
+                                    currentGameSetting = 'direction'
+                else:
+                    delta_X = mousePos[0] - razor[-1][0]
+                    delta_Y = mousePos[1] - razor[-1][1]
+                    if delta_X != 0:
+                        inclination = delta_Y / delta_X
+                    else:
+                        inclination = 100
 
+                    if angle(-30) < inclination < angle(30):
+                        if delta_X >= 0:
+                            direction = 0
+                        else:
+                            direction = 3
+                    elif angle(30) < inclination:
+                        if delta_X >= 0:
+                            direction = 1
+                        else:
+                            direction = 0
+                    elif angle(-30) > inclination:
+                        if delta_X >= 0:
+                            direction = 5
+                        else:
+                            direction = 2
 
+                    if razor[-1][-1] == direction:
+                        continue
+                    razor[-1].append(direction)
+                    if len(razor[-1]) == 4:
+                        currentGame = 'mirror'
+                        currentGameSetting = 'position'
 
+            elif currentGame == 'mirror':
+                if currentGameSetting == 'position':
+                    for i in boardPositionAll:
+                        for j in i:
+                                if j[0] - 5 < mousePos[0] < j[0] + 5 and j[1] - 5 < mousePos[1] < j[1] + 5:
+                                    if not (j in razor and j in mirror):
+                                        mirror.append(list(j))
+                                        print(mirror[-1])
+                                        currentGameSetting = 'direction'
+                else:
+                    delta_X = mousePos[0] - mirror[-1][0]
+                    delta_Y = mousePos[1] - mirror[-1][1]
+                    print(mousePos, mirror[-1])
+                    if delta_X != 0:
+                        inclination = delta_Y / delta_X
+                    else:
+                        inclination = 100
 
+                    if angle(-15) <= inclination < angle(15):
+                        direction = 0
+                    elif angle(15) <= inclination < angle(45):
+                        direction = 1
+                    elif angle(45) <= inclination < angle(75):
+                        direction = 2
+                    elif angle(75) <= inclination or angle(-75) <= inclination:
+                        direction = 3
+                    elif angle(-75) <= inclination < angle(-45):
+                        direction = 4
+                    else:
+                        direction = 5
+                    print(inclination, direction)
+                    mirror[-1].append(direction)
+                    currentGame = 'razor'
+                    currentGameSetting = 'position'
 
     pygame.display.flip()
     clock.tick(FPS)
